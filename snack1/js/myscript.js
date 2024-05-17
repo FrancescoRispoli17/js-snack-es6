6a214659e6e8b220b30ae1cbe3988b0e2b441ddc
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-17 11:27:02 +0200

    [UPD] snack 1

--- snack1/js/myscript.js
+++ snack1/js/myscript.js
@@ -11,15 +11,30 @@ const bikes=[
     },
     {
         bike: 'bike 3',
-        peso:10,
-    },
-    {
-        bike: 'bike 4',
         peso:13,
     },
     {
+        bike: 'bike 4',
+        peso:10,
+    },
+    {
         bike: 'bike 5',
-        peso:17,
+        peso:10,
     },
 ]
+let leggera=bikes[0].peso;
 
+for(let bike of bikes){
+    const attuale=bike.peso;
+
+    if(attuale<leggera)
+        leggera=attuale;
+}
+console.log(leggera);
+
+const bicileggere=[];
+for(let bike of bikes){
+    if(leggera===bike.peso)
+        bicileggere.push(bike);
+}
+console.log(bicileggere);
