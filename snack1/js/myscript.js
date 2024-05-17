2fc48fd9e6955197d8b0ecea623af10e63af79fb
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-17 10:50:07 +0200

    first commit

---
+++ snack1/js/myscript.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const bikes=[
+    {
+        bike: 'bike 1',
+        peso:20,
+    },
+    {
+        bike: 'bike 2',
+        peso:15,
+    },
+    {
+        bike: 'bike 3',
+        peso:10,
+    },
+    {
+        bike: 'bike 4',
+        peso:13,
+    },
+    {
+        bike: 'bike 5',
+        peso:17,
+    },
+]
+
